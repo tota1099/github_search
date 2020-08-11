1af9d633bf7625e5a95dba8c51cc386e508a169d
Renan Porto <renan.porto1099@gmail.com>
2020-08-11 17:34:33 -0300

    chore: add mockito

--- test/modules/search/domain/usecases/search_by_text_test.dart
+++ test/modules/search/domain/usecases/search_by_text_test.dart
@@ -1,13 +1,20 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:github_search/modules/search/domain/entities/result_search.dart';
+import 'package:github_search/modules/search/domain/repositories/search_repository.dart';
 import 'package:github_search/modules/search/domain/usecases/search_by_text.dart';
+import 'package:mockito/mockito.dart';
+
+class SearchRepositoryMock extends Mock implements SearchRepository {}
 
 main() {
 
-  final usecase = SearchByTextImpl();
+  final repository = SearchRepositoryMock();
+  final usecase = SearchByTextImpl(repository);
   
   test('Should return a list of ResultSearch', () async {
+    when(repository.search(any)).thenAnswer((_) async => Right(<ResultSearch>[]));
+
     final result = await usecase("renan");
     expect(result, isA<Right>());
     expect(result | null, isA<List<ResultSearch>>());
