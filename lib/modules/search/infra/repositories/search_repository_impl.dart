4d999873f6cc0567dbf6d422f1d4233b45880654
Renan Porto <renan.porto1099@gmail.com>
2020-08-11 21:34:57 -0300

    test: ensure SearchRepositoryImpl return a list of ResultSearch

---
+++ lib/modules/search/infra/repositories/search_repository_impl.dart
@@ -0,0 +1,17 @@
+import 'package:github_search/modules/search/domain/errors/errors.dart';
+import 'package:github_search/modules/search/domain/entities/result_search.dart';
+import 'package:dartz/dartz.dart';
+import 'package:github_search/modules/search/domain/repositories/search_repository.dart';
+import 'package:github_search/modules/search/infra/datasources/search_datasource.dart';
+
+class SearchRepositoryImpl implements SearchRepository {
+  final SearchDataSource datasource;
+
+  SearchRepositoryImpl(this.datasource);
+  
+  @override
+  Future<Either<FailureSearch, List<ResultSearch>>> search(String searchText) {
+    throw UnimplementedError();
+  }
+
+}
---
+++ test/modules/search/infra/repositories/search_repository_impl_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:github_search/modules/search/domain/entities/result_search.dart';
+import 'package:github_search/modules/search/infra/datasources/search_datasource.dart';
+import 'package:github_search/modules/search/infra/models/result_search_model.dart';
+import 'package:github_search/modules/search/infra/repositories/search_repository_impl.dart';
+import 'package:mockito/mockito.dart';
+
+class SearchDataSourceMock extends Mock implements SearchDataSource {}
+
+main() {
+  final datasource = SearchDataSourceMock();
+  final repository = SearchRepositoryImpl(datasource);
+
+  test('Should return a list of ResultSearch', () async {
+    when(datasource.getSearch(any)).thenAnswer((_) async => <ResultSearchModel>[]);
+    final result = await repository.search("renan");
+    
+    expect(result | null, isA<List<ResultSearch>>());
+  });
+}
