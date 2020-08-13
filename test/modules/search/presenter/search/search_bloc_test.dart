d4e7aac2e2f0788756cd594053c06fbd089c664f
Renan Porto <renan.porto1099@gmail.com>
2020-08-13 10:26:36 -0300

    test: ensure SearchBloc return the states in the correct order

---
+++ test/modules/search/presenter/search/search_bloc_test.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:github_search/modules/search/domain/entities/result_search.dart';
+import 'package:github_search/modules/search/domain/usecases/search_by_text.dart';
+import 'package:github_search/presenter/search/search_bloc.dart';
+import 'package:github_search/presenter/search/states/state.dart';
+import 'package:mockito/mockito.dart';
+
+class SearchByTextMock extends Mock implements SearchByText {}
+main() {
+
+  final usecase = SearchByTextMock();
+  final bloc =  SearchBloc(usecase);
+
+  test("Should return the states in the correct order", () {
+    when(usecase.call(any)).thenAnswer((_) async => Right(<ResultSearch>[]) );
+    expect(bloc, emitsInOrder([
+      isA<SearchLoading>(),
+      isA<SearchSuccess>()
+    ]));
+    bloc.add("renan");
+  });
+}
